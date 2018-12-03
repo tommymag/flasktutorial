9a7d0543015ea1768deed21e91169cc59b4ffce6
tom <t.mags21.tm@gmail.com>
2018-12-03 11:35:53 -0800

    Commit version0.3

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 BASEDIR=/Users/thomasmagnuson/Documents/PDXCodeGuild/advanced_portfolio/flaskapp 
-docker-compose -f ${BASEDIR}/docker-compose.yml up --build
+docker-compose -f ${BASEDIR}/docker-compose.yml up --build -d
