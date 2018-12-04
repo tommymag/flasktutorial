88b00fb68f9d00514dbc5d5b9d2a2ebc0bd3b6a8
tom <t.mags21.tm@gmail.com>
2018-12-04 12:04:58 -0800

    Commit version0.9

--- web/app/main/views.py
+++ web/app/main/views.py
@@ -2,31 +2,43 @@ import datetime
 import logging
 import os
 
-from flask import current_app, escape
+from flask import abort, current_app, escape, redirect, render_template, url_for
+from jinja2 import TemplateNotFound
 from . import main
 
 
-@main.route('/main/')
-def hello_main():
-    logging.info("hello_main()")
-    return 'Hello FlaskApp : Main Module'
+@main.route('/home/')
+def main_home():
+    return redirect(url_for('.main_page'))
+
+
+@main.route('/', defaults={'page': 'index'})
+@main.route('/<page>/')
+def main_page(page):
+    try:
+        logging.debug( "main_page( %s )" % page )
+        title = "FlaskApp Page"
+        return render_template('%s.html' % (page), page_title=title)
+    except TemplateNotFound:
+        logging.info('TemplateNotFound: %s.html' % (page))
+        abort(404)
 
 
 @main.route('/info/date')
 def info_date():
     ts = datetime.datetime.now().strftime("%Y/%m/%d @ %H:%M:%S")
-    return "Current Datetime : %s" % ts
+    return "Current Datetime :<pre> %s </pre>" % ts
 
 
 @main.route('/info/config')
 def info_config():
     cnf = dict(current_app.config)
-    return "'%s' Config : %s" % (os.getenv('FLASK_CONFIG'),cnf)
+    return "'%s' Config :<pre> %s </pre>" % (os.getenv('FLASK_CONFIG'),cnf)
 
 
 @main.route('/info/url_map')
 def info_url_map():
-    return "current_app.url_map:<pre> %s </pre>" % escape(current_app.url_map)
+    return "current_app.url_map :<pre> %s </pre>" % escape(current_app.url_map)
 
 
 
